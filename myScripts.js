6b8a917e34b021b82a4547d58359d516ddbfe7b4
BubenBescobar <55450928+BubenBescobar@users.noreply.github.com>
2020-01-15 11:54:19 -0500

    Update myScripts.js

--- myScripts.js
+++ myScripts.js
@@ -1,11 +1,11 @@
 var beginning = 10;
 var limit = 27;
-/*for(i = beginning ; i < limit ; i++){
+for(i = beginning ; i < limit ; i++){
   document.write ("Maf" + Math.sqrt(beginning) + "<br />");
   beginning++;
-}*/
+}
 
-/*while(beginning < limit ){
+while(beginning < limit ){
   document.write ("Maf" + Math.floor(Math.random() * 100) + "<br />");
   beginning ++;
-}*/
+}
